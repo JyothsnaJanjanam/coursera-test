874fb53d5039d3862b7d5bd723bfe5e52d990d03
JyothsnaJanjanam <110269771+JyothsnaJanjanam@users.noreply.github.com>
2022-08-26 19:56:00 +0530

    Delete SpeakGoodBoy.js

--- SpeakGoodBoy.js
+++
@@ -1,7 +0,0 @@
-(function(window){
-    var speakword="GoodBye ";
-    var speakgoodbye=(name) => {
-        console.log(speakword + name);
-    }
-    window.speakgoodbye=speakgoodbye;
-})(window);
